acbcbf9279b481be99c14b97fc2df7434aa13a6c
Snezroamia <lesterliew0@gmail.com>
2023-01-11 20:10:14 +0800

    Updated info on main.cpp

--- main.cpp
+++ main.cpp
@@ -2,10 +2,10 @@
 // Course: TCP1101 PROGRAMMING FUNDAMENTALS 
 // Year: Trimester 1, 2022/23 (T2215) 
 // Lab: TT1L 
-// Names: ANG KHAI PIN | LESTER LIEW 
-// IDs: 1211101248 | MEMBER_ID_2 |  
-// Emails: 1211101248@student.mmu.edu.my | MEMBER_EMAIL_2 | 
-// Phones: 011-57725120 | MEMBER_PHONE_2 | 
+// Names: ANG KHAI PIN | LESTER LIEW JUN MIN |
+// IDs: 1211101248 | 1221302721 |  
+// Emails: 1211101248@student.mmu.edu.my | 1221302721@student.mmu.edu.my | 
+// Phones: 011-57725120 | 012-9156393 | 
 // ********************************************************* 
 
 // TODO: Fill in the missing information above and delete this line.
