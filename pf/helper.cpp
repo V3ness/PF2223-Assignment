29774f2390d73088d8829ce726aaa00521e12d36
Veness Ang <89562242+V3ness@users.noreply.github.com>
2023-01-21 11:08:25 +0800

    Update helper.cpp

--- pf/helper.cpp
+++ pf/helper.cpp
@@ -4,10 +4,6 @@
 
 namespace pf
 {
-    const int kRows = 3;
-    const int kColumns = 5;
-    char kBoard[kRows][kColumns];
-
     int ClearScreen()
     {
         #if defined(_WIN32)
@@ -24,31 +20,5 @@ namespace pf
         #elif defined(__linux__) || defined(__APPLE__)
             return std::system(R"(read -p "Press any key to continue . . . " dummy)");
         #endif
-    }
-
-    void CreateGameBoard()
-    {
-        for (int row = 0; row < kRows; ++row)
-        for (int col = 0; col < kColumns; ++col)
-        {
-            int random_number = std::rand() % 2;
-            if (random_number)
-                kBoard[row][col] = '*';
-            else
-                kBoard[row][col] = 'r';
-        }
-    }
-
-    void ShowGameBoard()
-    {
-        for (int row = 0; row < kRows; ++row)
-        {
-            for (int col = 0; col < kColumns; ++col)
-            {
-                std::cout << kBoard[row][col];
-            }
-            std::cout << std::endl;
-        }
-            
     }
 }
