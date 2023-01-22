f1e7811c6d02e30818740cd8cb6a2cf3403d63c2
Veness Ang <89562242+V3ness@users.noreply.github.com>
2023-01-22 22:06:55 +0800

    Update header.h

--- pf/header.h
+++ pf/header.h
@@ -7,6 +7,20 @@ namespace pf
     int Pause();
 }
 
+class Player
+{
+    public:
+        int AlienHp, AlienAtk = 0;
+        void CreateAlien();
+};
+
+class Enemy
+{
+    public:
+        int ZombieHp, ZombieAtk, ZombieRange;
+        void CreateZombie();
+};
+
 char randomiseItems(char x);
 
 #endif
