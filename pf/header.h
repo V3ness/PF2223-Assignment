acb6604efc9e230495bf5b81168f452b9bb485b3
Veness Ang <89562242+V3ness@users.noreply.github.com>
2023-01-25 21:03:45 +0800

    Update and rename helper.h to header.h

---
+++ pf/header.h
@@ -0,0 +1,43 @@
+#ifndef PF_HELPER_H
+#define PF_HELPER_H
+#include <iostream>
+#include <windows.h>
+#include <time.h>
+#include <vector>
+#include <iterator>
+#include <string>
+#include <cmath>
+
+namespace pf
+{
+    int ClearScreen();
+    int Pause();
+}
+
+class Player
+{
+public:
+    int AlienAtk = 0;
+    int randomNum = rand() % 3 + 1;
+    int AlienHp = 100 + (randomNum * 50);
+};
+
+class Enemy
+{
+public:
+    int ZombieCount = 1;
+    int randomHp = rand() % 2 + 1;
+    int randomAtk = rand() % 2 + 1;
+    int randomRng = rand() % 5 + 1;
+    int ZombieHp = 100 + (randomHp * 50);
+    int ZombieAtk = 5 + (randomAtk * 5);
+    int ZombieRange = randomRng;
+    std::vector<int> ZombHpVec;
+    std::vector<int> ZombAtkVec;
+    std::vector<int> ZombRngVec;
+    void ZombieCreation();
+};
+
+char randomiseItems(char x);
+
+#endif
