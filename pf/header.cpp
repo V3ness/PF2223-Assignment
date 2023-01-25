2bbe379f29ac854048ccfa84bf55381528365b0e
Veness Ang <89562242+V3ness@users.noreply.github.com>
2023-01-25 21:03:22 +0800

    Update and rename helper.cpp to header.cpp

---
+++ pf/header.cpp
@@ -0,0 +1,45 @@
+#include "header.h"
+#include <cstdlib>
+#include <windows.h>
+#include <iostream>
+
+// g++ pf\*.cpp main.cpp -o main
+
+namespace pf 
+{ 
+    int ClearScreen()
+    {
+        #if defined(_WIN32)
+            return std::system("cls");
+        #elif defined(__linux__) || defined(__APPLE__)
+            return std::system("clear");
+        #endif
+    }
+
+    int Pause()
+    {
+        #if defined(_WIN32)
+            return std::system("pause");
+        #elif defined(__linux__) || defined(__APPLE__)
+            return std::system(R"(read -p "Press any key to continue . . . " dummy)");
+        #endif
+    }
+}
+
+void Enemy::ZombieCreation()
+{
+    for (int i = 0; i < ZombieCount; i++)
+    {
+        int randomHp = rand() % 2 + 1;
+        int randomAtk = rand() % 2 + 1;
+        int randomRng = rand() % 5 + 1;
+        int ZombieHp = 100 + (randomHp * 50);
+        int ZombieAtk = 5 + (randomAtk * 5);
+        int ZombieRange = randomRng;
+        ZombHpVec.push_back(ZombieHp);
+        ZombAtkVec.push_back(ZombieAtk);
+        ZombRngVec.push_back(ZombieRange);
+        std::cout << '\n' << "  Zombie " << i + 1 << " : Life " << ZombieHp << ", Attack  " << ZombieAtk << ", Range " << ZombieRange;
+    }
+}
+
