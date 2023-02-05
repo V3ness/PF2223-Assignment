849cadeaedde23bf42aefdcb075663251bbb822c
Veness Ang <89562242+V3ness@users.noreply.github.com>
2023-02-05 23:20:27 +0800

    improved on zombie movement

--- pf/header.h
+++ pf/header.h
@@ -27,6 +27,7 @@ public:
     char getObject(int x, int y) const;
     void setObject(int x, int y, char ch);
     void setZomPos(int x, int y, int z);
+    void CombatHUD();
 };
 
 class Player
@@ -46,6 +47,8 @@ class Player
         int randomNum = rand() % 3 + 1;
         int const AlienHp = 100 + (randomNum * 50);
         int const MaxAlienHp = AlienHp;
+        std::vector<int> AlienHpVec;
+        void AlienCreation();
         void InitialLanding(Map &map_, float x, float y);
         void AlienMove(Map &map_, std::string inp, int x, int y);
         void AlienPlacement(Map &map_);
@@ -60,6 +63,11 @@ class Enemy
     int EPosX_, EPosY_;
 
 public:
+    bool hitBorder;
+    bool hitObject;
+    bool hitAlien;
+    int PrevX;
+    int PrevY;
     int PosX;
     int PosY;
     int randomHp = rand() % 2 + 1;
@@ -74,6 +82,11 @@ public:
     std::vector<int> ZombRngVec;
     void ZombieCreation();
     void ZombieLanding(Map &map_, int x, int y);
+    void ZombieMove(Map &map_, int x, int y);
+    void upPos(Map &map_);
+    void downPos(Map &map_);
+    void leftPos(Map &map_);
+    void rightPos(Map &map_);
 };
 
 void replaceDot(Map &map_, int rows, int columns);
@@ -81,5 +94,4 @@ void replaceDot(Map &map_, int rows, int columns);
 char randomiseItems(char x);
 
 #endif
-
 
