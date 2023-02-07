859b29668e90ebe14aa9e408fe3d215cc64e8afd
Veness Ang <89562242+V3ness@users.noreply.github.com>
2023-02-07 15:47:17 +0800

    multi zombie movement update

--- pf/header.h
+++ pf/header.h
@@ -71,25 +71,27 @@ public:
     bool hitAlien;
     int PrevX;
     int PrevY;
-    int PosX;
-    int PosY;
     int randomHp = rand() % 2 + 1;
     int randomAtk = rand() % 2 + 1;
     int randomRng = rand() % 5 + 1;
-    int ZombieCount = 1;
+    int ZombieCount = 2;
     int ZombieHp = 100 + (randomHp * 50);
     int ZombieAtk = 5 + (randomAtk * 5);
     int ZombieRange = randomRng;
+    int count = 49; // use to display zombie on the gameboard
+    int n = 0; // use to display zombie no. on the HUD
+    std::vector<int> ZombPosX; // vector to store each zombie's position x
+    std::vector<int> ZombPosY; // vector to store each zombie's position y
     std::vector<int> ZombHpVec;
     std::vector<int> ZombAtkVec;
     std::vector<int> ZombRngVec;
     void ZombieCreation();
     void ZombieLanding(Map &map_, int x, int y);
     void ZombieMove(Map &map_, int x, int y);
-    void upPos(Map &map_);
-    void downPos(Map &map_);
-    void leftPos(Map &map_);
-    void rightPos(Map &map_);
+    void upPos(Map &map_, int x);
+    void downPos(Map &map_, int x);
+    void leftPos(Map &map_, int x);
+    void rightPos(Map &map_, int x);
 };
 
 void replaceDot(Map &map_, int rows, int columns);
